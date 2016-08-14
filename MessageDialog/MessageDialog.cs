19c3b9948de9c982ca7de028157e42f6e39a4aa2
Kun-Yi Li <airlanser@gmail.com>
2016-08-14 16:46:01 +0800

    Initial commit of MessageDialog

---
+++ MessageDialog/MessageDialog.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageDialog : MonoBehaviour
+{
+	public ScrollRect dialog;
+
+	void Start()
+	{
+		dialog.verticalNormalizedPosition = 0.0f;
+	}
+}
